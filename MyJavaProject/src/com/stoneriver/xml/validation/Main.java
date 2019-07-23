0e2be0d0d09a78b1c6b99f4a57cd1ca7a3b4b02b
RPLDELL545 <RPLDELL545@RPLDELL545-PC>
2019-07-22 21:24:50 -0500

    [MyJavaProject] Updated XML parsing

--- MyJavaProject/src/com/stoneriver/xml/validation/Main.java
+++ MyJavaProject/src/com/stoneriver/xml/validation/Main.java
@@ -11,15 +11,25 @@ import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-		
+
+		/*
+		 * Create a SAXParser instance. Use new instance Then create the parser
+		 */
 		SAXParserFactory spf = SAXParserFactory.newInstance();
+		// Create the parser from the factory instance
+		// two exceptions to the main method
 		SAXParser sp = spf.newSAXParser();
+
+		/*
+		 * Parse the XML document. Event based parsing it sends it to a handler . It need a
+		 * handler to Accepts 2 parameters. The file and the handler.
+		 */
 		
-		sp.parse("C:/GitHub/MyJava/MyJavaProject/src/com/stoneriver/xml/validation/intro.xml", 
-				new MyHandler());
-		
-	}//end of main
+		//set the file path and handler. The handler can implement certain interfaces. Throw exceptions to the mail
+		sp.parse("C:/GitHub/MyJava/MyJavaProject/src/com/stoneriver/xml/validation/intro.xml", new MyHandler());
+
+	}// end of main
 
 }
--- MyJavaProject/src/com/stoneriver/xml/validation/MyHandler.java
+++ MyJavaProject/src/com/stoneriver/xml/validation/MyHandler.java
@@ -4,8 +4,16 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+/*
+ * Create a handler class to parse XML document
+ * Extends DefaultHandler
+*/
 public class MyHandler extends DefaultHandler {
+	
+	//For specific values
+	private String actualNodeName;
 
+	// Starts at the beginning of the document.
 	@Override
 	public void startDocument() throws SAXException {
 		// super.startDocument();
@@ -13,41 +21,63 @@ public class MyHandler extends DefaultHandler {
 
 	}
 
+	// ends of the document
+	@Override
 	public void endDocument() throws SAXException {
 		System.out.println("Parsing Ends");
 
 	}
 
+	/*
+	 * 
+	 */
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 		// TODO Auto-generated method stub
 		// super.startElement(uri, localName, qName, attributes);
+		// qName is currently parsed elements
+		
+		this.actualNodeName = qName;
+		
 		System.out.println("Start Element: " + qName);
 
+		/*
+		 * This reads the parameters.attribute name-getLocalName and values-getValue
+		 */
 		for (int i = 0; i < attributes.getLength(); i++) {
 			System.out.println("  Attribute: " + attributes.getLocalName(i) + ": " + attributes.getValue(i));
 
-		}
+		} // end of for loop
 	}
 
+	// End of the element
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
 		// TODO Auto-generated method stub
 		// super.endElement(uri, localName, qName);
 		System.out.println("End Element: " + qName);
 	}
-	
+	/*
+	 * Read the content of the documents. Accepts 3 parameters.
+	 */
 	@Override
 	public void characters(char[] ch, int start, int length) throws SAXException {
 		// TODO Auto-generated method stub
-		//super.characters(ch, start, length);
+		// super.characters(ch, start, length);
 		
+		//Only retrieve the values for <name> element
+		if (!actualNodeName.equals("name")) {
+			return;
+		}
+		
+		
+		//Use the string constructor
 		String nodeValue = new String(ch, start, length);
 		
-		if (!nodeValue.trim().equals(""))
-		{
+		//Check if any content exist. Remove empty spaces. Writes out the value.
+		if (!nodeValue.trim().equals("")) {
 			System.out.println("Node value: " + nodeValue);
 		}
-	}
+	} //end of characters method
 
 }
